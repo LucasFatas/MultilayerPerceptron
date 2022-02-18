a6ef8877181740dd3231afd6b255c7321cd3fd56
LucasFatas <69412382+LucasFatas@users.noreply.github.com>
2022-02-18 14:50:00 +0100

    Created NeuralNetwork and Perceptron files with some functions defined structure
    NeuralNetwork and Perceptron entity files created. Added some function definitions that we will definitely need. Will definitely have to add more functions.

---
+++ NeuralNetwork.py
@@ -0,0 +1,16 @@
+import numpy as np
+
+
+class NeuralNetwork:
+
+    def __init__(self):
+        pass
+
+    def feedforward(self):
+        pass
+
+    def backpropagation(self):
+        pass
+
+    def activation_function(self):
+        pass
---
+++ Perceptron.py
@@ -0,0 +1,10 @@
+
+
+class Perceptron:
+
+    def __init__(self, weights, bias):
+        pass
+
+    def calculate_output(self, input):
+        pass
+
