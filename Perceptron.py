1f4cf00aa6d13da5d883d65dd1ef3311108c12e4
LucasFatas <69412382+LucasFatas@users.noreply.github.com>
2022-02-24 13:57:16 +0100

    output of perceptrons saved in perceptron for backpropagation

--- Perceptron.py
+++ Perceptron.py
@@ -5,12 +5,14 @@ class Perceptron:
     def __init__(self, weights, bias):
         self.weights = weights
         self.bias = bias
+        self.output = None
 
     def activation_function(self, z):
         return 1/(1 + np.exp(-z))
 
     def calculate_output(self, input):
-        return self.activation_function(np.dot(input, self.weights) + self.bias)
+        self.output = self.activation_function(np.dot(input, self.weights) + self.bias)
+        return self.output
 
     def toString(self):
         print("Weights: " + str(self.weights))
