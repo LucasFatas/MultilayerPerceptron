7fc79882550b41f56fd91d2de24d422915ad7ca2
LucasFatas <69412382+LucasFatas@users.noreply.github.com>
2022-02-25 14:04:55 +0100

    Also save z, the output before its put in the activation function

--- Perceptron.py
+++ Perceptron.py
@@ -5,12 +5,14 @@ class Perceptron:
     def __init__(self, weights, bias):
         self.weights = weights
         self.bias = bias
+        self.z = None
         self.output = None
 
     def activation_function(self, z):
         return 1/(1 + np.exp(-z))
 
     def calculate_output(self, input):
+        self.z = np.dot(input, self.weights) + self.bias
         self.output = self.activation_function(np.dot(input, self.weights) + self.bias)
         return self.output
 
