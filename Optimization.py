c0a0b8eab753e3dd2c7497f1726f02e80f6d431e
Ravi Snellenberg <r.snellenberg@student.tudelft.nl>
2022-02-25 16:56:14 +0100

    Add the posibility to choose the k in k-crossvalidation

--- Optimization.py
+++ Optimization.py
@@ -5,13 +5,13 @@ from NeuralNetwork import NeuralNetwork
 
 class Optimization:
 
-    def crossvalidation(self, neuralnetwork ,training, target):
+    def crossvalidation(self, neuralnetwork ,training, target, k):
         sample_size = len(training)
-        validation_size = sample_size / 20  # the k in k-crossvalidation is hardcoded to 20 here
+        validation_size = sample_size / k  # the k in k-crossvalidation
         validation_end = 0
         for x in range(10):  # do the cross-validation step 10 times and take the average since the initialization is random
             error1 = 0
-            for i in range(1, 21):  # this is the cross-validation step
+            for i in range(1, k+1):  # this is the cross-validation step
                 validation_begin = validation_end
                 validation_end = validation_size * i
                 validation_set = np.take(training, range(validation_begin, validation_end))
@@ -19,7 +19,7 @@ class Optimization:
                 validation_target = np.take(target, range(validation_begin, validation_end))
                 training_target = np.delete(target, range(validation_begin, validation_end), None)
                 error1 += neuralnetwork.train(training_set, training_target, validation_set,
-                                   validation_target) / 20  # for now I assume train returns error (might be changed later)
+                                   validation_target) / k  # for now I assume train returns error (might be changed later)
         return error1 / 10
 
     def find_optimal_neuron_amount(self, training, target):
