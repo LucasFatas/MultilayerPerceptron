80d2d2d0c3f8cade938b7916e9b5ed8a26c5616b
Martin Petrov <M.A.Petrov@student.tudelft.nl>
2022-02-25 17:30:04 +0100

    - added output accuracy function

--- Main.py
+++ Main.py
@@ -3,6 +3,13 @@ import numpy as np
 
 nn = NeuralNetwork([2, 3, 4])
 
+def outputAccuracyScore(neuralnetwork, features, targets):
+    predictions = NeuralNetwork.feedforward(neuralnetwork, features)
+    totalCorrect = 0
+    for j in len(targets):
+        if predictions[j] == features[j]:
+            totalCorrect = totalCorrect + 1
+    totalCorrect / len(targets)
 
 def train_network():
     features = np.genfromtxt("data/features.txt", delimiter=",")
